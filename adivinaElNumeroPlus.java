8985f1a09ad967ce12ace770385bb13cdfd01eba
juanantoniorodnah <juanantaoki@gmail.com>
2024-10-15 18:32:26 +0200

    Modify adivinaElNumeroPlus.java

--- adivinaElNumeroPlus.java
+++ adivinaElNumeroPlus.java
@@ -8,12 +8,14 @@ public class AdivinaElNumeroPlus {
 		Scanner sc = new Scanner(System.in);
 
 		String respuesta;
+		
+		System.out.println("Bienvenido al juego de Adivinar el número");
 
 		do {
 			double numeroDecimales = Math.random() * 100 + 1;
 			int numeroAdivinar = (int) numeroDecimales;
 
-			// System.out.println(numeroAdivinar);
+			System.out.println(numeroAdivinar);
 
 			int intentos = 0;
 			int intentosMaximos = 5;
@@ -21,30 +23,42 @@ public class AdivinaElNumeroPlus {
 			boolean seguir = true;
 
 			while (seguir) {
+
 				System.out.println("Dime un numero entre 1 y 100");
+				System.out.println("----------------------------");
 
 				int numero = sc.nextInt();
 				intentos++;
 
 				System.out.println("Intento numero " + intentos);
-
+				System.out.println();
+				
 				if (numero == numeroAdivinar) {
 					System.out.println("Enhorabuena, has acertado.");
-									
+					System.out.println();
+
+					seguir = false;
+
 				} else if (intentos >= intentosMaximos) {
 					System.out.println("Tu número de intentos ha sido superdado.");
+					System.out.println();
+					System.out.println("El numero que buscabas era " + numeroAdivinar + ".");
+					System.out.println();
 					
 					seguir = false;
-					
+
 				} else if (numeroAdivinar < numero) {
 					System.out.println("El número es menor");
+					System.out.println();
+					
 				} else {
 					System.out.println("El número es mayor");
+					System.out.println();
+
 				}
 			}
 			
-			System.out.println("El numero que buscabas era " + numeroAdivinar + ".");
-			
+			System.out.println("-------------------------------------------");
 			System.out.println("¿Te gustaría jugar otra vez? Responde (s/N)");
 
 			sc.nextLine();
